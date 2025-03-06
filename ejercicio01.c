8fb38a90768b79f894aa3c473457e3383f609469
PepsiConTennis <eheduardohernandez8@gmail.com>
2025-03-06 14:11:48 -0600

    ejercicio01.c

---
+++ ejercicio01.c
@@ -0,0 +1,18 @@
+//Eduardo Hernandez
+//Ejercicio
+#include <stdio.h>
+#include <stdlio.h>
+int main()
+{
+  int a=3;
+  int b=2;
+
+  if (a>b)
+
+  {
+    printf ("a es mayor");
+  
+  }
+  return 0;
+
+}
